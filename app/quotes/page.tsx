bd9ec1ac93da886c6add52caa1fa475e80fd3962
Mohamad-Mahdi Tasha <imwhdiiii@gmail.com>
2026-06-28 17:03:50 +0330

    added changes like last commit to quotes  page

--- app/quotes/page.tsx
+++ app/quotes/page.tsx
@@ -1,43 +1,16 @@
 // Codes by mahdi tasha
-// Forcing next.js to render this component as client side component
-"use client";
-
 // Importing part
 import Header from "@/component/header";
-import Pagination from "@/component/pagination";
-import Quote from "@/component/quote";
-import {
-   Alert,
-   AlertAction,
-   AlertDescription,
-   AlertTitle,
-} from "@/component/ui/alert";
-import { Button } from "@/component/ui/button";
-import {
-   Empty,
-   EmptyDescription,
-   EmptyHeader,
-   EmptyMedia,
-   EmptyTitle,
-} from "@/component/ui/empty";
-import { axiosInstance } from "@/lib/axios";
-import { GETQuoteType } from "@/type/api";
-import { useQuery } from "@tanstack/react-query";
-import { Loader2, ShoppingCart } from "lucide-react";
-import { useState } from "react";
+import type { Metadata } from "next";
+import QuotesContainer from "@/component/container/quotesContainer";
+
+// Defining metadata
+export const metadata: Metadata = {
+   title: "Quotes",
+};
 
 // Creating and exporting Quotes page as default
 export default function Quotes() {
-   // Defining hooks
-   const [skip, setSkip] = useState(0);
-   const quotes = useQuery<GETQuoteType>({
-      queryKey: ["quotes", skip],
-      queryFn: async () => {
-         const data = await axiosInstance.get(`/quotes?skip=${skip}&limit=9`);
-         return data.data;
-      },
-   });
-
    // Returning JSX
    return (
       <>
@@ -46,65 +19,7 @@ export default function Quotes() {
             <div className="prose dark:prose-invert prose-neutral w-full max-w-full mb-10">
                <h1>Quotes</h1>
             </div>
-            <main>
-               {quotes.isPending ? (
-                  <div className="h-[500px] flex items-center justify-center">
-                     <Loader2 className="size-8 animate-spin" />
-                  </div>
-               ) : quotes.isError ? (
-                  <Alert variant={"destructive"}>
-                     <AlertTitle>Error</AlertTitle>
-                     <AlertDescription>
-                        There was an error while trying to fetch the Quotes.
-                     </AlertDescription>
-                     <AlertAction>
-                        <Button onClick={() => quotes.refetch()}>
-                           Try again
-                        </Button>
-                     </AlertAction>
-                  </Alert>
-               ) : !quotes.isPending && !quotes.isError && quotes.data ? (
-                  quotes.data.total === 0 ? (
-                     <Empty>
-                        <EmptyHeader>
-                           <EmptyMedia>
-                              <ShoppingCart />
-                           </EmptyMedia>
-                           <EmptyTitle>Nothing to show</EmptyTitle>
-                           <EmptyDescription>
-                              The list is empty and there is nothing to show !
-                           </EmptyDescription>
-                        </EmptyHeader>
-                     </Empty>
-                  ) : (
-                     <>
-                        {quotes.isRefetching ? (
-                           <div className="h-[500px] flex items-center justify-center">
-                              <Loader2 className="size-8 animate-spin" />
-                           </div>
-                        ) : (
-                           <>
-                              <div className="grid lg:grid-cols-3 gap-5 mb-10 align-items-center">
-                                 {quotes.data.quotes.map((item, index) => (
-                                    <Quote data={item} key={index} />
-                                 ))}
-                              </div>
-                           </>
-                        )}
-                        <Pagination
-                           total={quotes.data.total}
-                           skip={quotes.data.skip}
-                           limit={quotes.data.limit}
-                           onPageChange={(page) => {
-                              setSkip(page);
-                           }}
-                        />
-                     </>
-                  )
-               ) : (
-                  false
-               )}
-            </main>
+            <QuotesContainer />
          </section>
       </>
    );
---
+++ component/container/quotesContainer.tsx
@@ -0,0 +1,100 @@
+// Codes by mahdi tasha
+// Forcing next.js to render this component as client side component
+"use client";
+
+// Importing part
+import Pagination from "@/component/pagination";
+import Quote from "@/component/quote";
+import {
+   Alert,
+   AlertAction,
+   AlertDescription,
+   AlertTitle,
+} from "@/component/ui/alert";
+import { Button } from "@/component/ui/button";
+import {
+   Empty,
+   EmptyDescription,
+   EmptyHeader,
+   EmptyMedia,
+   EmptyTitle,
+} from "@/component/ui/empty";
+import { axiosInstance } from "@/lib/axios";
+import { GETQuoteType } from "@/type/api";
+import { useQuery } from "@tanstack/react-query";
+import { Loader2, ShoppingCart } from "lucide-react";
+import { useState } from "react";
+
+// Creating and exporting QuotesContainer page as default
+export default function QuotesContainer() {
+   // Defining hooks
+   const [skip, setSkip] = useState(0);
+   const quotes = useQuery<GETQuoteType>({
+      queryKey: ["quotes", skip],
+      queryFn: async () => {
+         const data = await axiosInstance.get(`/quotes?skip=${skip}&limit=9`);
+         return data.data;
+      },
+   });
+
+   // Returning JSX
+   return (
+      <main>
+         {quotes.isPending ? (
+            <div className="h-[500px] flex items-center justify-center">
+               <Loader2 className="size-8 animate-spin" />
+            </div>
+         ) : quotes.isError ? (
+            <Alert variant={"destructive"}>
+               <AlertTitle>Error</AlertTitle>
+               <AlertDescription>
+                  There was an error while trying to fetch the Quotes.
+               </AlertDescription>
+               <AlertAction>
+                  <Button onClick={() => quotes.refetch()}>Try again</Button>
+               </AlertAction>
+            </Alert>
+         ) : !quotes.isPending && !quotes.isError && quotes.data ? (
+            quotes.data.total === 0 ? (
+               <Empty>
+                  <EmptyHeader>
+                     <EmptyMedia>
+                        <ShoppingCart />
+                     </EmptyMedia>
+                     <EmptyTitle>Nothing to show</EmptyTitle>
+                     <EmptyDescription>
+                        The list is empty and there is nothing to show !
+                     </EmptyDescription>
+                  </EmptyHeader>
+               </Empty>
+            ) : (
+               <>
+                  {quotes.isRefetching ? (
+                     <div className="h-[500px] flex items-center justify-center">
+                        <Loader2 className="size-8 animate-spin" />
+                     </div>
+                  ) : (
+                     <>
+                        <div className="grid lg:grid-cols-3 gap-5 mb-10 align-items-center">
+                           {quotes.data.quotes.map((item, index) => (
+                              <Quote data={item} key={index} />
+                           ))}
+                        </div>
+                     </>
+                  )}
+                  <Pagination
+                     total={quotes.data.total}
+                     skip={quotes.data.skip}
+                     limit={quotes.data.limit}
+                     onPageChange={(page) => {
+                        setSkip(page);
+                     }}
+                  />
+               </>
+            )
+         ) : (
+            false
+         )}
+      </main>
+   );
+}
