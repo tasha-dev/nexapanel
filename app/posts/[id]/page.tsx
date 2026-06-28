895d789f00cec8f3aff536ac362918f1303fea78
Mohamad-Mahdi Tasha <imwhdiiii@gmail.com>
2026-06-28 11:20:01 +0330

    completed single post page

--- app/posts/[id]/page.tsx
+++ app/posts/[id]/page.tsx
@@ -10,11 +10,13 @@ import {
    AlertDescription,
    AlertTitle,
 } from "@/component/ui/alert";
+import { Badge } from "@/component/ui/badge";
 import { Button } from "@/component/ui/button";
 import { axiosInstance } from "@/lib/axios";
 import { Post } from "@/type/general";
 import { useQuery } from "@tanstack/react-query";
-import { Loader2 } from "lucide-react";
+import { Hash, Loader2 } from "lucide-react";
+import Link from "next/link";
 import { useParams } from "next/navigation";
 
 // Creating and exporting Single Post page as default
@@ -55,7 +57,26 @@ export default function SinglePost() {
                   <>
                      <div className="prose dark:prose-invert prose-neutral lg:col-span-4 w-full max-w-full">
                         <h1>{post.data.title}</h1>
+                        <div className="flex flex-wrap gap-1">
+                           {post.data.tags.map((tag, index) => (
+                              <Badge asChild key={index}>
+                                 <Link
+                                    href={`/posts/tags/${tag}`}
+                                    className="no-underline"
+                                 >
+                                    <Hash />
+                                    {tag}
+                                 </Link>
+                              </Badge>
+                           ))}
+                        </div>
                         <p>{post.data.body}</p>
+                        <hr />
+                        <ul>
+                           <li>Views : {post.data.views}</li>
+                           <li>Likes : {post.data.reactions.likes}</li>
+                           <li>Dislikes : {post.data.reactions.dislikes}</li>
+                        </ul>
                      </div>
                   </>
                ) : (
