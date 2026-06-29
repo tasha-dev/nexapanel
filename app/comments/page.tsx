7f48d75c0bcee9f18e20818eae749c5a175e9861
Mohamad-Mahdi Tasha <imwhdiiii@gmail.com>
2026-06-29 10:41:15 +0330

    added comments container for the comment page lol

--- app/comments/page.tsx
+++ app/comments/page.tsx
@@ -1,44 +1,16 @@
 // Codes by mahdi tasha
-// Forcing next.js to render this component as client side component
-"use client";
-
-import Comment from "@/component/comment";
 // Importing part
+import CommentsContainer from "@/component/container/commentsContainer";
 import Header from "@/component/header";
-import Pagination from "@/component/pagination";
-import Todo from "@/component/todo";
-import {
-   Alert,
-   AlertAction,
-   AlertDescription,
-   AlertTitle,
-} from "@/component/ui/alert";
-import { Button } from "@/component/ui/button";
-import {
-   Empty,
-   EmptyDescription,
-   EmptyHeader,
-   EmptyMedia,
-   EmptyTitle,
-} from "@/component/ui/empty";
-import { axiosInstance } from "@/lib/axios";
-import { GETCommentsType } from "@/type/api";
-import { useQuery } from "@tanstack/react-query";
-import { Loader2, ShoppingCart } from "lucide-react";
-import { useState } from "react";
+import type { Metadata } from "next";
+
+// Defining metadata
+export const metadata: Metadata = {
+   title: "Comments",
+};
 
 // Creating and exporting Comments page as default
 export default function Comments() {
-   // Defining hooks
-   const [skip, setSkip] = useState(0);
-   const comments = useQuery<GETCommentsType>({
-      queryKey: ["comments", skip],
-      queryFn: async () => {
-         const data = await axiosInstance.get(`/comments?skip=${skip}&limit=9`);
-         return data.data;
-      },
-   });
-
    // Returning JSX
    return (
       <>
@@ -47,65 +19,7 @@ export default function Comments() {
             <div className="prose dark:prose-invert prose-neutral w-full max-w-full mb-10">
                <h1>Comments</h1>
             </div>
-            <main>
-               {comments.isPending ? (
-                  <div className="h-[500px] flex items-center justify-center">
-                     <Loader2 className="size-8 animate-spin" />
-                  </div>
-               ) : comments.isError ? (
-                  <Alert variant={"destructive"}>
-                     <AlertTitle>Error</AlertTitle>
-                     <AlertDescription>
-                        There was an error while trying to fetch the Comments.
-                     </AlertDescription>
-                     <AlertAction>
-                        <Button onClick={() => comments.refetch()}>
-                           Try again
-                        </Button>
-                     </AlertAction>
-                  </Alert>
-               ) : !comments.isPending && !comments.isError && comments.data ? (
-                  comments.data.total === 0 ? (
-                     <Empty>
-                        <EmptyHeader>
-                           <EmptyMedia>
-                              <ShoppingCart />
-                           </EmptyMedia>
-                           <EmptyTitle>Nothing to show</EmptyTitle>
-                           <EmptyDescription>
-                              The list is empty and there is nothing to show !
-                           </EmptyDescription>
-                        </EmptyHeader>
-                     </Empty>
-                  ) : (
-                     <>
-                        {comments.isRefetching ? (
-                           <div className="h-[500px] flex items-center justify-center">
-                              <Loader2 className="size-8 animate-spin" />
-                           </div>
-                        ) : (
-                           <>
-                              <div className="grid lg:grid-cols-3 gap-5 mb-10 align-items-center">
-                                 {comments.data.comments.map((item, index) => (
-                                    <Comment data={item} key={index} />
-                                 ))}
-                              </div>
-                           </>
-                        )}
-                        <Pagination
-                           total={comments.data.total}
-                           skip={comments.data.skip}
-                           limit={comments.data.limit}
-                           onPageChange={(page) => {
-                              setSkip(page);
-                           }}
-                        />
-                     </>
-                  )
-               ) : (
-                  false
-               )}
-            </main>
+            <CommentsContainer />
          </section>
       </>
    );
