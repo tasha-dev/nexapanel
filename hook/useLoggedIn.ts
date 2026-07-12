e03fd5c2d24ee2b04cb5fdc3953196474ac2e7d3
Mohamad-Mahdi Tasha <imwhdiiii@gmail.com>
2026-07-12 12:02:59 +0330

    completed useLoggedIn custom hook to remove local storage items on some conditions

--- hook/useLoggedIn.ts
+++ hook/useLoggedIn.ts
@@ -3,11 +3,15 @@
 import { axiosInstance } from "@/lib/axios";
 import { GETMeType } from "@/type/api";
 import { useQuery } from "@tanstack/react-query";
+import { AxiosError } from "axios";
+import { useRouter } from "next/navigation";
+import { useEffect } from "react";
 
 // Creating and exporting useLoggedIn hook as default
 export default function useLoggedIn() {
    // Defining hooks
-   const { isPending, isError, data } = useQuery<GETMeType>({
+   const router = useRouter();
+   const { isPending, isError, data, ...query } = useQuery<GETMeType>({
       queryKey: ["me"],
       queryFn: async () => {
          const { data } = await axiosInstance.get("/auth/me");
@@ -16,6 +20,26 @@ export default function useLoggedIn() {
       retry: false,
       staleTime: Infinity,
    });
+
+   // Defining variables
+   const error = query.error as AxiosError;
+
+   // Using useEffect to remove local storage for access and refresh tokens when the fetching returns 401 (unauthorized)
+   useEffect(() => {
+      if (!isPending && isError && error) {
+         if (Number(error.stack) === 401) {
+            const accessToken = localStorage.getItem("accessToken");
+            const refreshToken = localStorage.getItem("refreshToken");
+
+            if (accessToken && refreshToken) {
+               localStorage.removeItem("accessToken");
+               localStorage.removeItem("refreshToken");
+
+               router.refresh();
+            }
+         }
+      }
+   }, [isPending]);
 
    // Returning part
    return {
